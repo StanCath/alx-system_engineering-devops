950e69feea0dc1c6a1a5327b862b3f048f3d6189
Stanley <san.cathline@gmail.com>
2022-10-19 06:11:24 -0700

    updated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-# Textme
-puts ARGV[0]
-       .scan(/(?<=from:|to:|flags:).*?(?=\])/)
-       .join(',')#!/usr/bin/env ruby
-# Textme
-puts ARGV[0]
-       .scan(/(?<=from:|to:|flags:).*?(?=\])/)
-       .join(',')
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
